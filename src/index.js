5637be12e0f2df2780a7e4f8e99f502a0a7acc60
Saumya Singh <singhsaumya201@gmail.com>
2024-12-21 14:50:12 +0530

    project start

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // Use 'react-dom/client' in React 18
+import "jquery";
+import "popper.js/dist/umd/popper";
+import "bootstrap/dist/js/bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.css"
+import "./index.css" ;
+
+
+// Create the root container
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render the React component
+root.render(<div>Hello</div>);
